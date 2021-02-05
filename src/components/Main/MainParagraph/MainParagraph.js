ba5936af641f77b9bd38e8b0b2813ba36a8cf86d
BrennnanPredmore <brennnann@gmail.com>
2021-02-05 17:16:55 -0600

    mainParagraph resized and refactored for mediaQueries

--- src/components/Main/MainParagraph/MainParagraph.js
+++ src/components/Main/MainParagraph/MainParagraph.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MainParagraph = () => {
   return (
     <div>
-      <p className='mainParagraph'>
+      <p className='containter mainParagraph'>
         Southwest Education has been working with schools in the Western United
         States for the past 25 years to help all students reach their potential
         and improve academic performance at each school. The SW Team of
