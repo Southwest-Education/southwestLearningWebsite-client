ef0ac220dfc21575f02b19769a3d4ccfc8e2d93f
BrennnanPredmore <brennnann@gmail.com>
2021-02-09 14:55:20 -0600

    mkdir components/Videoes // init all new files for each video

---
+++ src/components/Videos/ReadingPlusTestimonial/ReadingPlusTestimonialTwo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const ReadingPlusTestimonialTwo = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ReadingPlusTestimonialTwo
---
+++ src/components/Videos/SymphonyMathTestimonial/SymphonyMathTestimonialOne.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const SymphonyMathTestimonialOne = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SymphonyMathTestimonialOne
