79c9033a4d45fd37040a3fc9bb7419bd20b0661f
BrennnanPredmore <brennnann@gmail.com>
2021-02-10 11:47:14 -0600

    edit paragraphOne // init paragraphTwo

--- src/components/Pages/Products/Testimonials/Paragraph/ReadingPlusParagraph/ReadingPlusParagraphOne.js
+++ src/components/Pages/Products/Testimonials/Paragraph/ReadingPlusParagraph/ReadingPlusParagraphOne.js
@@ -5,12 +5,13 @@ const ReadingPlusParagraphOne = () => {
   return (
     <div>
       <p className='ReadingPlusParagraph'>
-        <b className='testimonialPerson'>Prairie Lea:</b> ISD has been on the Needs Improvement list for
-        three years.  In 2018, after implementing Reading Plus for all second
-        through twelfth grade students Prairie Lea received a “B” rating.  Not
-        only did they meet standard in all areas -but they received
-        a distinction in Reading! <br/>  Great Job Prairie Lea! Larry Markert,
-        Superintendent
+        <b className='testimonialPerson'>Prairie Lea ISD</b> "Prairie has been
+        on the Needs Improvement list for three years.  In 2018, after
+        implementing Reading Plus for all second through twelfth grade
+        students Prairie Lea received a “B” rating.  Not only did they meet
+        standard in all areas but they received a distinction in Reading! Great
+        Job Prairie Lea!"
+        <br /> Larry Markert, Superintendent
       </p>
     </div>
   );
--- src/components/Pages/Products/Testimonials/Paragraph/ReadingPlusParagraph/ReadingPlusParagraphTwo.js
+++ src/components/Pages/Products/Testimonials/Paragraph/ReadingPlusParagraph/ReadingPlusParagraphTwo.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './style.css';
 
 const ReadingPlusParagraphTwo = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <p className='ReadingPlusParagraph'>
+        <b className='testimonialPerson'>Iowa Park CISD</b>  “Approximately 70%
+        of all students have shown improvement since the start of the school
+        year. Our 8th graders had 91% passing rate, up 3% in just a few months
+        on the Reading Plus program. We had several students who had never
+        passed the state test until we had them use the Reading Plus program.
+        They all passed.”
+        <br /> Darla Biddy, Principal WF George Middle School
+      </p>
+    </div>
+  );
+};
 
-export default ReadingPlusParagraphTwo
+export default ReadingPlusParagraphTwo;
