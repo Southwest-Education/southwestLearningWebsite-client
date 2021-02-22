3561383269686430c4ff1b98888e34c66c1850f8
shawn220a <shawn.42496@gmail.com>
2021-02-22 14:24:12 -0600

    Change class component structure to state component

--- src/components/Pages/Company/WhereWeReach/WhereWeReach.js
+++ src/components/Pages/Company/WhereWeReach/WhereWeReach.js
@@ -1,77 +1,83 @@
-import React, { Component } from 'react';
-import './style.css';
-import ReactMapGL, { Marker, NavigationControl } from 'react-map-gl';
-import axios from 'axios';
-import { faMapMarkerAlt } from '@fortawesome/pro-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from 'mapbox-gl';
+import React, { useState } from "react";
+import "./style.css";
+import ReactMapGL, { Marker, NavigationControl } from "react-map-gl";
+import axios from "axios";
+import { faMapMarkerAlt } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
-export default class WhereWeReach extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewport: {
-        width: '100%',
-        height: '65vh',
-        latitude: 39.8283,
-        longitude: -98.5795,
-        zoom: 3.75,
-      },
-      data: [],
-    };
-  }
+const WhereWeReach = () => {
+  return <div></div>;
+};
 
-  componentDidMount() {
-    axios.get('/api/map').then((res) => {
-      const data = res.data;
-      this.setState({ data: data });
-    });
-  }
+export default WhereWeReach;
 
-  render() {
-    const { viewport, data } = this.state;
+// export default class WhereWeReach extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       viewport: {
+//         width: '100%',
+//         height: '65vh',
+//         latitude: 39.8283,
+//         longitude: -98.5795,
+//         zoom: 3.75,
+//       },
+//       data: [],
+//     };
+//   }
 
-    return (
-      <div className="container">
-        <ReactMapGL
-          width={viewport.width}
-          height={viewport.height}
-          latitude={viewport.latitude}
-          longitude={viewport.longitude}
-          zoom={viewport.zoom}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
-          onViewportChange={(viewport) => {
-            this.setState({ viewport });
-          }}
-        >
-          <NavigationControl className="navControlStyle" />
-          {data.map((mapData) =>
-            mapData.geometry.map((coord) => {
-              return (
-                <Marker
-                  key={coord._id}
-                  latitude={coord.lat}
-                  longitude={coord.lon}
-                  offsetLeft={-24.33}
-                  offsetTop={-26.5}
-                >
-                  <FontAwesomeIcon
-                    className="markerIcon"
-                    size="2x"
-                    icon={faMapMarkerAlt}
-                  />
-                </Marker>
-              );
-            })
-          )}
-        </ReactMapGL>
-        <hr className="whereWeReach" />
-      </div>
-    );
-  }
-}
+//   componentDidMount() {
+//     axios.get('/api/map').then((res) => {
+//       const data = res.data;
+//       this.setState({ data: data });
+//     });
+//   }
+
+//   render() {
+//     const { viewport, data } = this.state;
+
+//     return (
+//       <div className="container">
+//         <ReactMapGL
+//           width={viewport.width}
+//           height={viewport.height}
+//           latitude={viewport.latitude}
+//           longitude={viewport.longitude}
+//           zoom={viewport.zoom}
+//           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+//           mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
+//           onViewportChange={(viewport) => {
+//             this.setState({ viewport });
+//           }}
+//         >
+//           <NavigationControl className="navControlStyle" />
+//           {data.map((mapData) =>
+//             mapData.geometry.map((coord) => {
+//               return (
+//                 <Marker
+//                   key={coord._id}
+//                   latitude={coord.lat}
+//                   longitude={coord.lon}
+//                   offsetLeft={-24.33}
+//                   offsetTop={-26.5}
+//                 >
+//                   <FontAwesomeIcon
+//                     className="markerIcon"
+//                     size="2x"
+//                     icon={faMapMarkerAlt}
+//                   />
+//                 </Marker>
+//               );
+//             })
+//           )}
+//         </ReactMapGL>
+//         <hr className="whereWeReach" />
+//       </div>
+//     );
+//   }
+// }
