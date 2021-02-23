6392597f0ee4be6e2ff4fe8d354b274e8f65635c
shawn220a <shawn.42496@gmail.com>
2021-02-23 03:11:11 -0600

    Move rest of commented out code for refactor out of MapBoxMarker.js to FormFields.js

--- src/components/Forms/FormFields.js
+++ src/components/Forms/FormFields.js
@@ -181,6 +181,65 @@ export const ContactFormFields = ({ url }) => {
 };
 
 export const MapDataFormFields = ({ url }) => {
+  //     this.state = {
+  //       name: '',
+  //       streetAddress: '',
+  //       city: '',
+  //       state: '',
+  //       zipCode: '',
+  //       districtName: '',
+  //       districtStreetAddress: '',
+  //       districtCity: '',
+  //       districtState: '',
+  //       districtZipCode: '',
+  //       lon: '',
+  //       lat: '',
+  //     };
+
+  //   handleSubmit = async (e) => {
+  //     e.preventDefault();
+
+  //     const {
+  //       name,
+  //       streetAddress,
+  //       city,
+  //       state,
+  //       zipCode,
+  //       districtName,
+  //       districtStreetAddress,
+  //       districtCity,
+  //       districtState,
+  //       districtZipCode,
+  //       lon,
+  //       lat,
+  //     } = this.state;
+
+  //     // Clear form data
+  //     e.target.reset();
+
+  //     await axios.post(url,
+  //       {
+  //         name,
+  //         streetAddress,
+  //         city,
+  //         state,
+  //         zipCode,
+  //         districtName,
+  //         districtStreetAddress,
+  //         districtCity,
+  //         districtState,
+  //         districtZipCode,
+  //         lon,
+  //         lat,
+  //       },
+  //       {
+  //         headers: {
+  //           'content-type': 'application/json',
+  //         },
+  //       },
+  //     );
+  //   };
+
   return (
     <div>
       <Form style={textStyle} variant='bg-light' onSubmit={this.handleSubmit}>
