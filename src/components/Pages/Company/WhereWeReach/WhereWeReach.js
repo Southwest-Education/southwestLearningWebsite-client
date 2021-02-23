bbd2c6b176341a97f6365e3a35722c07282f803c
shawn220a <shawn.42496@gmail.com>
2021-02-22 19:10:12 -0600

    Add popup to each marker on click

--- src/components/Pages/Company/WhereWeReach/WhereWeReach.js
+++ src/components/Pages/Company/WhereWeReach/WhereWeReach.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
-import ReactMapGL, { Marker, NavigationControl } from "react-map-gl";
+import ReactMapGL, { Marker, NavigationControl, Popup } from "react-map-gl";
 import axios from "axios";
 import { faMapMarkerAlt } from "@fortawesome/pro-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -20,6 +20,8 @@ const WhereWeReach = () => {
   });
 
   const [mapData, getMapData] = useState([]);
+
+  const [selectedBuilding, setSelectedBuilding] = useState(null);
 
   useEffect(() => {
     getAllMapData();
@@ -58,11 +60,29 @@ const WhereWeReach = () => {
                   className="markerIcon"
                   size="2x"
                   icon={faMapMarkerAlt}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setSelectedBuilding(data);
+                  }}
                 />
               </Marker>
             );
           })
         )}
+        {selectedBuilding ? (
+          <Popup
+            latitude={selectedBuilding.geometry[0].lat}
+            longitude={selectedBuilding.geometry[0].lon}
+            onClose={() => {
+              setSelectedBuilding(null);
+            }}
+          >
+            <div>
+              <h2>{selectedBuilding.name}</h2>
+              <p>{selectedBuilding.district[0].name}</p>
+            </div>
+          </Popup>
+        ) : null}
       </ReactMapGL>
       <hr className="whereWeReach" />
     </div>
