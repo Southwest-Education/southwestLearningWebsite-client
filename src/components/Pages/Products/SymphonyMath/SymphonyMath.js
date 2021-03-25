6e5dfdc22b92e49744132ed3d09e06f79d25002f
BrennnanPredmore <brennnann@gmail.com>
2021-03-25 00:33:55 -0500

    video resize // line height added to right side DIV

--- src/components/Pages/Products/SymphonyMath/SymphonyMath.js
+++ src/components/Pages/Products/SymphonyMath/SymphonyMath.js
@@ -13,18 +13,15 @@ import DivOneText2 from './DivOneText2/DivOneText2';
 import { RedirectButton } from '../../../Buttons/Buttons';
 
 const SymphonyMath = () => {
-  const centerLine = {
-    borderRight: '0.1875rem solid',
-    borderRightColor: '#f97c05',
-  };
   return (
     <div>
       <div>
         <Header />
         <DivOneText />
         <div className='row'>
+          
           {/* LEFT SIDE */}
-          <div style={centerLine} className='col-sm-6'>
+          <div className='col-sm-6'>
             <SubHeader1 />
             <DivOneText2 />
             {/* BUTTONS */}
@@ -39,7 +36,6 @@ const SymphonyMath = () => {
               </div>
             </div>
           </div>
-
           {/* RIGHT SIDE */}
           <div className='col-sm-6'>
             <SubHeader2 />
@@ -47,8 +43,8 @@ const SymphonyMath = () => {
               <ReactPlayer
                 url='https://youtu.be/24WOuguyUlc'
                 controls={true}
-                width='67%'
-                height='35vh'
+                width='100%'
+                height='50vh'
               />
             </div>
             <div className='col-sm-12 d-flex justify-content-center'>
