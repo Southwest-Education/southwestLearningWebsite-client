ef75a31c1a9d4f4c33ad27e0dab398178ffe3912
BrennnanPredmore <brennnann@gmail.com>
2021-07-21 12:09:52 -0500

    typo error fixed on reading plus div on main page

--- src/components/Main/DivTwo/DivTwo.js
+++ src/components/Main/DivTwo/DivTwo.js
@@ -18,7 +18,7 @@ const DivTwo = () => {
             The Reading Plus program focuses on improving and changing the way
             students read. Reading Plus targets Silent Reading Skills, Fluency,
             Comprehension, Academic Vocabulary, College-Career Readiness Skills
-            and Writing. Reading Plus is web-based and can be use 24/7 from
+            and Writing. Reading Plus is web-based and can be used 24/7 from
             anywhere at anytime.
           </Card.Text>
           <div className='cardButton'>
